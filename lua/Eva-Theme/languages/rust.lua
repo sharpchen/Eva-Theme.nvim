d982ba1fca53cc4b6d29daaec988f7499f6d59f5
sharpchen <rui.chen.sharp@gmail.com>
2024-06-05 04:13:14 +0800

    better rust

--- lua/Eva-Theme/languages/rust.lua
+++ lua/Eva-Theme/languages/rust.lua
@@ -3,6 +3,7 @@ local function rust(h)
     h:map_token('logical', '@lsp.typemod.keyword.controlFlow.rust')
         :map_token('primitive', '@lsp.type.builtinType.rust')
         :map_token('type', '@lsp.type.typeAlias.rust')
+        :map_token('property', '@module.rust')
 end
 
 return rust
