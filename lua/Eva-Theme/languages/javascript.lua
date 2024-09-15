9ae3ea2ed994d464c07f3e8d9cce8c10b7304b53
sharpchen <rui.chen.sharp@gmail.com>
2024-09-15 11:35:11 +0800

    chore(javascript): remove scope

--- lua/Eva-Theme/languages/javascript.lua
+++ lua/Eva-Theme/languages/javascript.lua
@@ -4,7 +4,6 @@ local function javascript(h)
     :map_token('func', { '@punctuation.special.javascript' }, function(p, as) -- string interpolation
       return { fg = p[as], nocombine = true }
     end)
-    :map_token('declarative', '@keyword.operator.javascript')
     :map_token('property', '@lsp.type.namespace.javascript')
 end
 
