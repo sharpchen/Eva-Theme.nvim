ed8c874d1b5f9dbf87d7a188844ccdf7bcb719b6
sharpchen <rui.chen.sharp@gmail.com>
2024-09-27 11:55:45 +0800

    chore(html): fix heading color

--- lua/Eva-Theme/languages/html.lua
+++ lua/Eva-Theme/languages/html.lua
@@ -1,9 +1,11 @@
 local utils = require('Eva-Theme.utils')
 ---@type StaticImporter
 local function html(h)
-  h:map_token('text', '@string.special.url.html'):map_token('NONE', '@tag.delimiter.html', function(palette, _)
-    return { fg = utils.is_dark(palette) and '#838FA7' or '#727376' }
-  end)
+  h:map_token('text', '@string.special.url.html')
+    :map_token('NONE', '@tag.delimiter.html', function(palette, _)
+      return { fg = utils.is_dark(palette) and '#838FA7' or '#727376' }
+    end)
+    :map_token('variable', '@markup.heading.html')
 end
 
 return html
