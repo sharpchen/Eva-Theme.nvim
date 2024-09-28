6fea3b9773dd60dacd4ab78c8d59f7ddb645c6bc
sharpchen <rui.chen.sharp@gmail.com>
2024-09-28 13:36:15 +0800

    chore(css): fix color

--- lua/Eva-Theme/languages/css.lua
+++ lua/Eva-Theme/languages/css.lua
@@ -1,7 +1,14 @@
 ---@type StaticImporter
 local function css(h)
-    h:map_token('parameter', '@variable.css')
-        :map_token('instanceReference', '@keyword.modifier.css')
+  h:map_token('parameter', '@variable.css')
+    :map_token('instanceReference', '@keyword.modifier.css')
+    :map_token('text', '@type.css')
+    :map_token('NONE', '@property.css', function(palette, _)
+      return { fg = palette.escape }
+    end)
+    :map_token('NONE', '@operator.css', function(palette, _)
+      return { fg = palette.punctuation }
+    end)
 end
 
 return css
