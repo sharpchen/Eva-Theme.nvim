c99bad8d080a3f55cdc070d320cc69624620c1dc
sharpchen <rui.chen.sharp@gmail.com>
2024-10-11 20:43:04 +0800

    fix(markdown): add italic style

--- lua/Eva-Theme/languages/markdown.lua
+++ lua/Eva-Theme/languages/markdown.lua
@@ -1,6 +1,8 @@
 ---@type StaticImporter
 local function markdown(h)
-    h:map_token('text', 'markdownUrl')
+  h:map_token('text', 'markdownUrl'):map_token('NONE', '@markup.italic', function(_, _)
+    return { italic = true, fg = '#C57BDB' }
+  end)
 end
 
 return markdown
