5705af6a09f8a0a6947c89e395ee866106f45239
sharpchen <rui.chen.sharp@gmail.com>
2024-10-15 15:00:03 +0800

    chore(render-markdown): add RenderMarkdownCodeInline

--- lua/Eva-Theme/ui/render-markdown.lua
+++ lua/Eva-Theme/ui/render-markdown.lua
@@ -1,7 +1,10 @@
+local utils = require('Eva-Theme.utils')
 ---@type StaticImporter
 local function render_markdown(h)
   h:map_ui('panelBackground', 'RenderMarkdownCode', function(palette, as)
     return { bg = palette[as] }
+  end):map_ui('NONE', 'RenderMarkdownCodeInline', function(p, _)
+    return { fg = p.text, bg = utils.is_dark(p) and '#495949' or '#bed7c5' }
   end)
 end
 return render_markdown
