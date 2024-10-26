38c05e2fd3a586fee73ed621c6cd38713edd9356
sharpchen <rui.chen.sharp@gmail.com>
2024-10-26 23:43:40 +0800

    chore(powershell): add colors

--- lua/Eva-Theme/languages/powershell.lua
+++ lua/Eva-Theme/languages/powershell.lua
@@ -1,6 +1,13 @@
 ---@type StaticImporter
 local function powershell(h)
   h:map_token('func', '@function.builtin.powershell')
+    :map_token('type', '@type.builtin.powershell')
+    :map_token('property', '@variable.builtin.powershell')
+    :map_token('logical', '@operator.powershell')
+    :map_token('NONE', '@variable.parameter.builtin.powershell', function(_, _)
+      return { fg = '#C57BDB' }
+    end)
+    :map_token('text', '@variable.parameter.powershell')
 end
 
 return powershell
