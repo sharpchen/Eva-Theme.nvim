9225f41dcb60db8bbc8628a667b9cdad3b34ec51
sharpchen <rui.chen.sharp@gmail.com>
2024-10-26 23:56:12 +0800

    chore(bash): add colors

--- lua/Eva-Theme/languages/bash.lua
+++ lua/Eva-Theme/languages/bash.lua
@@ -1,6 +1,8 @@
 ---@type StaticImporter
 local function bash(h)
   h:map_token('func', '@function.builtin.bash')
+    :map_token('property', '@variable.builtin.bash')
+    :map_token('text', '@variable.parameter.bash')
 end
 
 return bash
