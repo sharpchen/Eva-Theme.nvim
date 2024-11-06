ff6bb4050ba6a82e4fc5fce56f820496c8ae3016
sharpchen <rui.chen.sharp@gmail.com>
2024-11-06 19:11:13 +0800

    chore(dap-ui): add colors

--- lua/Eva-Theme/ui/dap-ui.lua
+++ lua/Eva-Theme/ui/dap-ui.lua
@@ -1,13 +1,25 @@
+local utils = require('Eva-Theme.utils')
 ---@type StaticImporter
 local function dap_ui(h)
-  h:map_token('type', 'DapUIType')
-    :map_token('func', { 'DapUIDecoration', 'DapUIScope', 'DapUIStoppedThread' })
-    :map_token('text', { 'DapUIValue', 'DapUILineNumber', 'DapUIBreakpointsPath' })
-    :map_token('NONE', 'DapUIModifiedValue', function(p, _)
+  h:map_ui(
+    'type',
+    { 'DapUIType', 'DapUIStepOverNC', 'DapUIStepIntoNC', 'DapUIStepOutNC', 'DapUIStepBackNC' },
+    function(p, as)
+      return { fg = p[as], bg = p.background, bold = true }
+    end
+  )
+    :map_ui('func', { 'DapUIDecoration', 'DapUIScope', 'DapUIStoppedThread' })
+    :map_ui('text', { 'DapUIValue', 'DapUILineNumber', 'DapUIBreakpointsPath' })
+    :map_ui('NONE', 'DapUIModifiedValue', function(p, _)
       return { fg = p.git.diffModified }
     end)
-    :map_token('logical', { 'DapUIWatchesValue', 'DapUICurrentFrameName' })
-    :map_token('parameter', 'DapUIBreakpointsCurrentLine')
+    :map_ui('logical', { 'DapUIWatchesValue', 'DapUICurrentFrameName' })
+    :map_ui('parameter', 'DapUIBreakpointsCurrentLine')
+    :map_ui('error', 'DapUIStopNC')
+    :map_ui('text', { 'DapUIRestartNC', 'DapUIPlayPause' })
+    :map_ui('warning', 'debugPC', function(p, as)
+      return { bg = utils.is_dark(p) and '#463D3A' or '#E7DBD4' }
+    end)
 end
 
 return dap_ui
