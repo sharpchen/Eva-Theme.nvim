b7dd87eacb17e91443ff081aef11ee3b2237552d
sharpchen <rui.chen.sharp@gmail.com>
2024-11-06 17:04:17 +0800

    feat(ui): add dap-ui

---
+++ lua/Eva-Theme/ui/dap-ui.lua
@@ -0,0 +1,12 @@
+---@type StaticImporter
+local function dap_ui(h)
+  h:map_token('type', 'DapUIType')
+    :map_token('func', { 'DapUIDecoration', 'DapUIScope', 'DapUIStoppedThread' })
+    :map_token('text', { 'DapUIValue', 'DapUILineNumber', 'DapUIBreakpointsPath' })
+    :map_token('NONE', 'DapUIModifiedValue', function(p, _)
+      return { fg = p.git.diffModified }
+    end)
+    :map_token('logical', { 'DapUIWatchesValue', 'DapUICurrentFrameName' })
+end
+
+return dap_ui
