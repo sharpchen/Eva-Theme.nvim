b9b2a6fa268db53f81aec723ac2dc325d02f456a
sharpchen <rui.chen.sharp@gmail.com>
2025-02-18 14:08:46 +0800

    chore(pwsh): cancel color

--- lua/Eva-Theme/languages/powershell.lua
+++ lua/Eva-Theme/languages/powershell.lua
@@ -3,7 +3,6 @@ local function powershell(h)
   h:map_token('func', '@function.builtin.powershell')
     :map_token('primitive', '@type.builtin.powershell')
     :map_token('property', '@variable.builtin.powershell')
-    :map_token('logical', '@operator.powershell')
     :map_token('NONE', '@variable.parameter.builtin.powershell', function(_, _)
       return { fg = '#C57BDB' }
     end)
