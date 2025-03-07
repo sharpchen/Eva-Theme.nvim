efb226d8db096c3d7413321f469000fb04126277
sharpchen <rui.chen.sharp@gmail.com>
2025-03-08 00:57:40 +0800

    refactor(sniprun): links to inline diagnostic

--- lua/Eva-Theme/ui/sniprun.lua
+++ lua/Eva-Theme/ui/sniprun.lua
@@ -1,13 +1,10 @@
 local utils = require('Eva-Theme.utils')
 ---@type StaticImporter
 local function sniprun(h)
-  h:map_ui('NONE', 'SniprunVirtualTextOk', function(palette, _)
-    return {
-      fg = utils.is_dark(palette) and '#8A97C3' or '#888888',
-      bg = utils.is_dark(palette) and '#343744' or '#e5e8ee',
-    }
-  end):map_ui('NONE', 'SniprunVirtualTextErr', function(p, _)
-    return { fg = utils.is_dark(p) and '#F36464' or '#E45454', bg = utils.is_dark(p) and '#3D3037' or '#EBDAE0' }
+  h:map_ui('NONE', 'SniprunVirtualTextOk', function(_, _)
+    return { link = 'LspInlayHint' }
+  end):map_ui('NONE', 'SniprunVirtualTextErr', function(_, _)
+    return { link = 'DiagnosticVirtualTextError' }
   end)
 end
 
