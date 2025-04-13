b67737b3cfdd85cb6ddd079962bf11e24fabad19
sharpchen <rui.chen.sharp@gmail.com>
2025-04-13 09:01:04 +0800

    chore(illuminate): add highlight

--- lua/Eva-Theme/ui/vim-illuminate.lua
+++ lua/Eva-Theme/ui/vim-illuminate.lua
@@ -3,6 +3,8 @@ local utils = require('Eva-Theme.utils')
 local function vim_illuminate(h)
   h:map_ui('NONE', { 'IlluminatedWordText', 'IlluminatedWordRead', 'IlluminatedWordWrite' }, function(palette, _)
     return { bg = utils.is_dark(palette) and '#17596B' or '#7FD8DE' }
+  end):map_ui('NONE', 'IlluminatedWordWrite', function(p, _)
+    return { bg = utils.is_dark(p) and '#66609f' or '#c9bff2' }
   end)
 end
 
