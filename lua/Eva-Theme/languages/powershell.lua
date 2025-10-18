ade1adc39a9208652b7ba351fa4820572687a199
sharpchen <rui.chen.sharp@gmail.com>
2025-10-18 20:04:28 +0800

    chore(pwsh): add highlight

--- lua/Eva-Theme/languages/powershell.lua
+++ lua/Eva-Theme/languages/powershell.lua
@@ -3,7 +3,7 @@ local function powershell(h)
   h:map_token('func', '@function.builtin.powershell')
     :map_token('primitive', '@type.builtin.powershell')
     :map_token('property', '@variable.builtin.powershell')
-    :map_token('NONE', '@variable.parameter.builtin.powershell', function(_, _)
+    :map_token('NONE', { '@variable.parameter.builtin.powershell', '@lsp.type.parameter.ps1' }, function(_, _)
       return { fg = '#C57BDB' }
     end)
     :map_token('text', '@variable.parameter.powershell')
